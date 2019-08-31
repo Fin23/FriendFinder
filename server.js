669a7066eb5597e322a72fc03084450ee92add98
Ross Jameson <Rossjameson1623@gmail.com>
2019-08-31 11:01:41 -0500

    working on html files, creating questions, need to get the results to display

--- server.js
+++ server.js
@@ -1,48 +1,42 @@
-//var express = require("express");
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
 
-//var path = require("path");
-//var app = express();
-
-// Require/import the HTTP module
-var http = require("http");
-
-// Define a port to listen for incoming requests
-var PORT = 7500;
+var express = require("express");
+var exphbs = require("express-handlebars");
+var mysql = require("mysql");
 
 
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-// Use the Node HTTP package to create our server.
-// Pass the handleRequest function to empower it with functionality.
-var server = http.createServer(handleRequest);
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
 
-// Routes
-// =============================================================
+ require("./app/routing/apiRoutes")(app);
+ require("./app/routing/htmlRoutes")(app);
 
-// Basic route that sends the user first to the AJAX Page
-// app.get("/", function(req, res) {
-//     res.sendFile(path.join(__dirname, "view.html"));
-//   });
-  
-//   app.get("/add", function(req, res) {
-//     res.sendFile(path.join(__dirname, "add.html"));
-//   });
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
 
-// Create a generic function to handle requests and responses
-function handleRequest(request, response) {
-
-  // Send the below string to the client when the user visits the PORT URL
-  response.end("It Works!! Path Hit: " + request.url);
-}
-
-
-
-// Start our server so that it can begin listening to client requests.
-server.listen(PORT, function() {
-
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
 });
